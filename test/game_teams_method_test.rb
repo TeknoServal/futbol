8bec9c4d5ce9bb06043415f53743e4df0402c005
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-14 18:56:23 -0400

    Refactored tests with setup

--- test/game_teams_method_test.rb
+++ test/game_teams_method_test.rb
@@ -16,107 +16,69 @@ class GameTeamsMethodsTest < Minitest::Test
     }
 
     @stat_tracker = StatTracker.new(locations)
+
+    @game_teams = './data/game_teams.csv'
+    @game_teams_methods = GameTeamsMethods.new(@game_teams, @stat_tracker)
   end
 
   def test_it_exists
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
+    assert_instance_of GameTeamsMethods, @game_teams_methods
 
-    assert_instance_of GameTeamsMethods, game_teams_methods
-
-    assert_equal './data/game_teams.csv', game_teams_methods.game_teams
+    assert_equal './data/game_teams.csv', @game_teams_methods.game_teams
   end
 
   def test_it_can_generate_array_of_game_teams_objects
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_instance_of Array, game_teams_methods.create_array(game_teams)
-    assert_instance_of GameTeams, game_teams_methods.all_game_teams[0]
-    assert_instance_of GameTeams, game_teams_methods.all_game_teams[999]
+    assert_instance_of Array, @game_teams_methods.create_array(@game_teams)
+    assert_instance_of GameTeams, @game_teams_methods.all_game_teams[0]
+    assert_instance_of GameTeams, @game_teams_methods.all_game_teams[999]
   end
 
   def test_it_can_return_best_offense_team
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "54", game_teams_methods.best_offense_team
+    assert_equal "54", @game_teams_methods.best_offense_team
   end
 
   def test_it_will_make_a_hash_of_team_id_and_goal_array
-    game_teams = './data/game_teams.csv'
-
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_instance_of Hash, game_teams_methods.assign_goals_by_teams
-    assert_equal true, game_teams_methods.assign_goals_by_teams.keys.include?("3")
-    assert_equal true, game_teams_methods.assign_goals_by_teams.keys.include?("6")
+    assert_instance_of Hash, @game_teams_methods.assign_goals_by_teams
+    assert_equal true, @game_teams_methods.assign_goals_by_teams.keys.include?("3")
+    assert_equal true, @game_teams_methods.assign_goals_by_teams.keys.include?("6")
   end
 
   def test_it_can_return_a_hash_of_teams_and_average_goal
-    game_teams = './data/game_teams.csv'
-
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_instance_of Hash, game_teams_methods.average_goals_by_team
+    assert_instance_of Hash, @game_teams_methods.average_goals_by_team
   end
 
   def test_it_can_return_worst_offense_team
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "7", game_teams_methods.worst_offense_team
+    assert_equal "7", @game_teams_methods.worst_offense_team
   end
 
   def test_it_can_get_highest_scoring_visitor_team
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "6", game_teams_methods.highest_scoring_team("away")
+    assert_equal "6", @game_teams_methods.highest_scoring_team("away")
   end
 
   def test_it_can_get_highest_scoring_home_team
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "54", game_teams_methods.highest_scoring_team("home")
+    assert_equal "54", @game_teams_methods.highest_scoring_team("home")
   end
 
   def test_it_can_get_lowest_scoring_visitor_team
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "27", game_teams_methods.lowest_scoring_team("away")
+    assert_equal "27", @game_teams_methods.lowest_scoring_team("away")
   end
 
   def test_it_can_get_lowest_scoring_home_team
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "7", game_teams_methods.lowest_scoring_team("home")
+    assert_equal "7", @game_teams_methods.lowest_scoring_team("home")
   end
 
   def test_it_can_get_team_with_most_tackles
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "FC Cincinnati", game_teams_methods.most_tackles("20132014")
-    assert_equal "Seattle Sounders FC", game_teams_methods.most_tackles("20142015")
+    assert_equal "FC Cincinnati", @game_teams_methods.most_tackles("20132014")
+    assert_equal "Seattle Sounders FC", @game_teams_methods.most_tackles("20142015")
   end
 
   def test_it_can_get_team_with_the_fewest_tackles
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
-    assert_equal "Atlanta United", game_teams_methods.fewest_tackles("20132014")
-    assert_equal "Orlando City SC", game_teams_methods.fewest_tackles("20142015")
+    assert_equal "Atlanta United", @game_teams_methods.fewest_tackles("20132014")
+    assert_equal "Orlando City SC", @game_teams_methods.fewest_tackles("20142015")
   end
 
   def test_it_can_return_a_hash_of_team_id_and_total_tackles
-    game_teams = './data/game_teams.csv'
-    game_teams_methods = GameTeamsMethods.new(game_teams, @stat_tracker)
-
     assert_equal ["16",1836],
-    game_teams_methods.assign_tackles_by_season("20132014").first
+    @game_teams_methods.assign_tackles_by_season("20132014").first
   end
 end
