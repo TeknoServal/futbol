6f0b64bf178fd8a5b4111f00e1b7ed7c39aff6f9
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-15 17:31:59 -0600

    Refactor and add team and team test

--- lib/team_methods.rb
+++ lib/team_methods.rb
@@ -1,25 +1,28 @@
 require 'CSV'
+require_relative './team'
 
 class TeamMethods
-  attr_reader :teams_array, :teams
+  attr_reader :teams, :file_loc
 
-  def initialize(teams)
-    @teams = teams
-    @teams_array = create_table(@teams)
+  def initialize(file_loc)
+    @file_loc = file_loc
+    @teams = create_teams(@file_loc)
   end
 
-  def create_table(file)
-    CSV.parse(File.read(file), headers: true)
+  def create_teams(file)
+    CSV.parse(File.read(file), headers: true).map do |row|
+      Team.new(row)
+    end
   end
 
   def count_of_teams
-    @teams_array['teamName'].count
+    @teams.count
   end
 
   def find_by_id(team_id)
-    team = @teams_array.find do |team|
-      team['team_id'] == team_id
+    team = @teams.find do |team|
+      team.team_id == team_id
     end
-    team['teamName']
+    team.team_name
   end
 end
--- test/team_methods_test.rb
+++ test/team_methods_test.rb
@@ -10,7 +10,7 @@ class TeamMethodsTest < Minitest::Test
 
     assert_instance_of TeamMethods, team_methods
 
-    assert_equal './data/teams.csv', team_methods.teams
+    assert_equal './data/teams.csv', team_methods.file_loc
   end
 
   def test_it_can_count_teams
@@ -29,5 +29,21 @@ class TeamMethodsTest < Minitest::Test
     assert_equal 'Reign FC', team_methods.find_by_id("54")
     assert_equal 'Sporting Kansas City', team_methods.find_by_id("5")
     assert_equal 'Houston Dynamo', team_methods.find_by_id("3")
+  end
+
+  def test_team_info
+    teams = './data/teams.csv'
+
+    team_methods = TeamMethods.new(teams)
+
+    expected = {
+      team_id: 1,
+      franchise_id: 23,
+      team_name: "Atlanta United",
+      abbreviation: "ATL",
+      link: "/api/v1/teams/1"
+    }
+
+    assert_equal expected, team_methods.team_info
   end
 end
