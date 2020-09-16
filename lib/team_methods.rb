c78d6b2f2d250ababf80493867c6d450ef99d96b
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-16 13:58:27 -0600

    Add least goals scored method

--- lib/team_methods.rb
+++ lib/team_methods.rb
@@ -92,4 +92,15 @@ class TeamMethods
       end
     end.max
   end
+
+  def least_goals_scored(team_id)
+    played_games = games_played(team_id, @stat_tracker.game_methods.games)
+    played_games.map do |game|
+      if team_id == game.home_team_id
+        game.home_goals.to_i
+      else 
+        game.away_goals.to_i
+      end
+    end.min
+  end
 end
